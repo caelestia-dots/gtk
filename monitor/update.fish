d9a7bc00f18e54815e93464ef2dc349ce1749fe8
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-02-23 20:31:02 +1100

    Change light/dark mode + icon theme

--- monitor/update.fish
+++ monitor/update.fish
@@ -5,6 +5,7 @@ set -q XDG_STATE_HOME && set -l state $XDG_STATE_HOME || set -l state $HOME/.loc
 set -l scheme_path $state/caelestia/scheme/current.txt
 set -l schemes (dirname (status filename))/../schemes
 
+# Update GTK+ theme colours
 cp $schemes/template.css $schemes/current.css
 cat $scheme_path | while read line
     set -l colour (string split ' ' $line)
@@ -13,6 +14,27 @@ end
 
 cp $schemes/current.css $config/gtk-3.0/gtk.css
 cp $schemes/current.css $config/gtk-4.0/gtk.css
+
+# Update GTK+ icon theme
+set -l colour_scheme (cat $state/caelestia/scheme/current-mode.txt)
+gsettings set org.gnome.desktop.interface color-scheme \'prefer-$colour_scheme\'
+
+set -l icon_themes /usr/share/icons/* ~/.icons/* ~/.local/share/icons/*
+set i 1
+while test $i -le (count $icon_themes)
+    if test ! -d $icon_themes[$i] -o -d $icon_themes[$i]/cursors
+        # Filter out cursor themes
+        set -e icon_themes[$i]
+    else
+        # Get icon theme name
+        set icon_themes[$i] (basename $icon_themes[$i])
+        set i (math $i + 1)
+    end
+end
+
+set -l icon_theme (gsettings get org.gnome.desktop.interface icon-theme | string sub -s 2 -e -1 | string replace -ri -- '-(dark|light)' '')
+set -l mode_theme (string match -i $icon_theme-$colour_scheme $icon_themes)
+test -n "$mode_theme" && gsettings set org.gnome.desktop.interface icon-theme \'$mode_theme[1]\'
 
 # Reload GTK+ theme (not working)
 set -l theme (gsettings get org.gnome.desktop.interface gtk-theme)
